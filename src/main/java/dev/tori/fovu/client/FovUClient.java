df788a4c7ab055c6bc0a06c059cc7544c9fffd27
7orivorian <7orivorian+git@gmail.com>
2025-07-29 17:16:59 -0400

    Initial commit

---
+++ src/main/java/dev/tori/fovu/client/FovUClient.java
@@ -0,0 +1,10 @@
+package dev.tori.fovu.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class FovunboundClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
---
+++ src/main/java/dev/tori/fovu/mixin/client/MixinGameRenderer.java
@@ -0,0 +1,7 @@
+package dev.tori.fovu.mixin.client;/**
+ * @author <a href="https://github.com/7orivorian">7orivorian</a>
+ * @since 1.0.0
+ */
+public class MixinGameRenderer {
+
+}
