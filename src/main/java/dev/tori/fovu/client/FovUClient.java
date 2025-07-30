f75af98663c2ae9e191bab9732c6c4e357a9cbf4
7orivorian <7orivorian+git@gmail.com>
2025-07-29 22:12:54 -0400

    Update mod ID, package names, and dependencies; implement custom FOV adjustments
    - Change mod ID to `fovu` and update associated package paths.
    - Add custom FOV slider logic with validation and clamping.
    - Update dependencies, Gradle configuration, and metadata for Minecraft version compatibility.

--- src/main/java/dev/tori/fovu/client/FovUClient.java
+++ src/main/java/dev/tori/fovu/client/FovUClient.java
@@ -2,9 +2,17 @@ package dev.tori.fovu.client;
 
 import net.fabricmc.api.ClientModInitializer;
 
-public class FovunboundClient implements ClientModInitializer {
+public class FovUClient implements ClientModInitializer {
+
+    public static final String MOD_ID = "fovu";
+    public static final String MOD_NAME = "FOVUnbound";
+    public static final String MOD_VERSION = "1.0.0";
+
+    public static final int MIN_FOV = 1;
+    public static final int MAX_FOV = 170;
 
     @Override
     public void onInitializeClient() {
+
     }
 }
--- src/main/java/dev/tori/fovu/mixin/client/MixinGameRenderer.java
+++ src/main/java/dev/tori/fovu/mixin/client/MixinGameRenderer.java
@@ -1,7 +1,29 @@
-package dev.tori.fovu.mixin.client;/**
+package dev.tori.fovu.mixin.client;
+
+import dev.tori.fovu.client.FovUClient;
+import net.minecraft.client.render.Camera;
+import net.minecraft.client.render.GameRenderer;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+/**
  * @author <a href="https://github.com/7orivorian">7orivorian</a>
  * @since 1.0.0
  */
+@Mixin(GameRenderer.class)
 public class MixinGameRenderer {
 
+    @Inject(
+            method = "getFov(Lnet/minecraft/client/render/Camera;FZ)F",
+            at = @At(
+                    value = "RETURN"
+            ),
+            cancellable = true
+    )
+    private void getFov(Camera camera, float tickProgress, boolean changingFov, CallbackInfoReturnable<Float> cir) {
+        final float clampedFov = Math.clamp(cir.getReturnValue(), FovUClient.MIN_FOV, FovUClient.MAX_FOV);
+        cir.setReturnValue(clampedFov);
+    }
 }
