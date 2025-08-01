e9de8b6e9a242dcee4d7cf48cd89fda217bc8020
7orivorian <7orivorian+git@gmail.com>
2025-08-01 00:34:31 -0400

    Fix mixin compatibility with zoom mods

--- src/main/java/dev/tori/fovu/mixin/client/MixinGameRenderer.java
+++ src/main/java/dev/tori/fovu/mixin/client/MixinGameRenderer.java
@@ -1,12 +1,10 @@
 package dev.tori.fovu.mixin.client;
 
+import com.llamalad7.mixinextras.injector.ModifyReturnValue;
 import dev.tori.fovu.client.FovUClient;
-import net.minecraft.client.render.Camera;
 import net.minecraft.client.render.GameRenderer;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 /**
  * @author <a href="https://github.com/7orivorian">7orivorian</a>
@@ -15,15 +13,13 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 @Mixin(GameRenderer.class)
 public class MixinGameRenderer {
 
-    @Inject(
+    @ModifyReturnValue(
             method = "getFov(Lnet/minecraft/client/render/Camera;FZ)F",
             at = @At(
-                    value = "RETURN"
-            ),
-            cancellable = true
+                    "RETURN"
+            )
     )
-    private void getFov(Camera camera, float tickProgress, boolean changingFov, CallbackInfoReturnable<Float> cir) {
-        final float clampedFov = Math.clamp(cir.getReturnValue(), FovUClient.MIN_FOV, FovUClient.MAX_FOV);
-        cir.setReturnValue(clampedFov);
+    private float getFov(float fov) {
+        return Math.clamp(fov, FovUClient.MIN_FOV, FovUClient.MAX_FOV);
     }
 }
