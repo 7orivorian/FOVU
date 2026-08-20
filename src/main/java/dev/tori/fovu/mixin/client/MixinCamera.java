55c3eedeb1331f69527263fe7bd6e9bcfc16e543
7orivorian <61598620+7orivorian@users.noreply.github.com>
2026-08-20 16:50:44 -0400

    Update to Minecraft version 26.1

--- src/main/java/dev/tori/fovu/mixin/client/MixinCamera.java
+++ src/main/java/dev/tori/fovu/mixin/client/MixinCamera.java
@@ -2,7 +2,7 @@ package dev.tori.fovu.mixin.client;
 
 import com.llamalad7.mixinextras.injector.ModifyReturnValue;
 import dev.tori.fovu.client.FovUClient;
-import net.minecraft.client.render.GameRenderer;
+import net.minecraft.client.Camera;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 
@@ -10,16 +10,26 @@ import org.spongepowered.asm.mixin.injection.At;
  * @author <a href="https://github.com/7orivorian">7orivorian</a>
  * @since 1.0.0
  */
-@Mixin(GameRenderer.class)
-public class MixinGameRenderer {
+@Mixin(Camera.class)
+public class MixinCamera {
 
     @ModifyReturnValue(
-            method = "getFov(Lnet/minecraft/client/render/Camera;FZ)F",
+            method = "getFov",
             at = @At(
                     "RETURN"
             )
     )
-    private float getFov(float fov) {
+    private float getFov_Return(float fov) {
         return Math.clamp(fov, FovUClient.MIN_FOV, FovUClient.MAX_FOV);
+    }
+
+    @ModifyReturnValue(
+            method = "calculateFov(F)F",
+            at = @At(
+                    "RETURN"
+            )
+    )
+    private float calculateFov_Return(float original) {
+        return Math.clamp(original, FovUClient.MIN_FOV, FovUClient.MAX_FOV);
     }
 }
---
+++ src/main/java/dev/tori/fovu/mixin/client/MixinOptions.java
@@ -0,0 +1,71 @@
+package dev.tori.fovu.mixin.client;
+
+import com.mojang.serialization.Codec;
+import dev.tori.fovu.client.FovUClient;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.OptionInstance;
+import net.minecraft.client.Options;
+import net.minecraft.client.renderer.LevelRenderer;
+import net.minecraft.network.chat.Component;
+import org.objectweb.asm.Opcodes;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Mutable;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.io.File;
+import java.util.function.Consumer;
+
+/**
+ * @author <a href="https://github.com/7orivorian">7orivorian</a>
+ * @since 1.0.0
+ */
+@Mixin(Options.class)
+public abstract class MixinOptions {
+
+    @Mutable
+    @Shadow
+    @Final
+    private OptionInstance<Integer> fov;
+
+    @Shadow
+    private static void operateOnLevelRenderer(final Consumer<LevelRenderer> consumer) {
+
+    }
+
+    @Inject(
+            method = "<init>(Lnet/minecraft/client/Minecraft;Ljava/io/File;)V",
+            at = @At(
+                    value = "FIELD",
+                    target = "Lnet/minecraft/client/Options;fov:Lnet/minecraft/client/OptionInstance;",
+                    shift = At.Shift.AFTER,
+                    opcode = Opcodes.PUTFIELD
+            )
+    )
+    private void onGameOptionsInit(Minecraft minecraft, File workingDirectory, CallbackInfo ci) {
+        this.fov = new OptionInstance<>(
+                "options.fov",
+                OptionInstance.noTooltip(),
+                (caption, value) -> {
+                    Component component;
+                    switch (value) {
+                        case 70 -> component = Options.genericValueLabel(caption, Component.translatable("options.fov.min"));
+                        case 110 -> component = Options.genericValueLabel(caption, Component.translatable("options.fov.max"));
+                        default -> component = Options.genericValueLabel(caption, value);
+                    }
+
+                    return component;
+                },
+                new OptionInstance.IntRange(FovUClient.MIN_FOV, FovUClient.MAX_FOV),
+                Codec.DOUBLE.xmap(
+                        (value) -> (int) (value * (double) 40.0F + (double) 70.0F),
+                        (value) -> ((double) value - (double) 70.0F) / (double) 40.0F
+                ),
+                70,
+                _ -> operateOnLevelRenderer(LevelRenderer::needsUpdate)
+        );
+    }
+}
